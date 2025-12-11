0945ec97d686f73e576f6ae75c4a9b2516ab61bd
Agidafmnez <senemannah@gmail.com>
2025-12-11 12:13:11 +0800

    Configured DATABASE_URL for Railway deployment

--- config/database.js
+++ config/database.js
@@ -1,13 +1,12 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql', // change to 'postgres' if using PostgreSQL
-  logging: false
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
 });
-
-sequelize.authenticate()
-  .then(() => console.log('Database connected'))
-  .catch(err => console.error('Unable to connect to DB:', err));
 
 module.exports = sequelize;
